5cd8c7d40599bc4fbc12d8708248253a528bee68
mariusz7b1 <35932429+mariusz7b1@users.noreply.github.com>
2024-12-03 13:49:46 +0100

    dzien2srodek

---
+++ Dzień 2/Przyklady/05_Konwertowanie/03_outgridview.ps1
@@ -0,0 +1,4 @@
+﻿$wybrani=Get-ADUser -Filter * | select name,surname,givenname | Out-GridView -OutputMode Multiple
+$wybrany=Get-ADUser -Filter * | select name,surname,givenname | Out-GridView -OutputMode Single
+$wybrany | FT
+$wybrani | FT
