9dde482ddfe0c2bdd082c0e3f5c8e3e77157bd29
mariusz7b1 <35932429+mariusz7b1@users.noreply.github.com>
2024-12-04 13:40:28 +0100

    Update 04_parametry.ps1

--- Dzień 3/Przyklady/06_Parametry_Skryptu/04_parametry.ps1
+++ Dzień 3/Przyklady/06_Parametry_Skryptu/04_parametry.ps1
@@ -1,6 +1,26 @@
-﻿Param (
-    [Parameter(Mandatory=$true)][string]$napis
-    )
+﻿<#
+.SYNOPSIS
+prezentuje parametry wywołania skryptu
+.DESCRIPTION
+Użyj skryptu jako wzorca do modyfikacji 
+mamy do czynienia z 3 parametrami, jeden obligatoryjny
+.PARAMETER ComputerName
+nazwa komputera
+.PARAMETER EventID  
+parametr typu int
+.EXAMPLE
+.\moj_skrypt -ComputerName LON-DC1 
+.EXAMPLE
+.\moj_skrypt -ComputerName LON-DC1 12 
+#>
 
-Write-Host "Wartość parametru to: $napis" -NoNewline -BackgroundColor Black -ForegroundColor Green
 
+Param (
+    [Parameter(mandatory=$true)] [string]$ComputerName,
+    [int]$EventID,
+    [Switch] $OnOff
+)
+
+Write-Host "Wartość parametru 'ComputerName' to: $ComputerName"
+Write-Host "Wartość parametru 'EventID' to: $EventID"
+Write-Host "Wartość parametru 'OnOf' to: $OnOff"
